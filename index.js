7c1a66ba6b3073b57087b77c29004b0b9a79d8e0
chicobanza0-ctrl <chicobanza0@gmail.com>
2026-07-14 12:15:52 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('✅ Servidor FichaCerta funcionando!');
+});
+
+const PORTA = process.env.PORT || 3000;
+app.listen(PORTA, () => {
+  console.log(`Servidor rodando na porta ${PORTA}`);
+});
